8c72f55d94eb930540cf700b8d61e952ce3b82b1
Sergio Azconan <sergio.j.azcona@gmail.com>
2022-06-08 13:39:05 -0400

    Added updated b/c new var not in previous copy

--- variables.rb
+++ variables.rb
@@ -4,6 +4,8 @@ hotel = "Hilton"
 person_1 = "Logan"
 person_2 = "Ari"
 
+puts hotel
+
 #integers
 hour_person_1 = 2
 hour_person_2 = 3
@@ -11,11 +13,23 @@ amount = 16
 
 #floats
 remaining_total = 16000.00
-per_year = 72350.32
+found = 72500.32
 desire = 10500.30
-diff = 10500.30 - 72350.32
+diff = 10500.30 - 72500.32
+puts diff
 
 #booleans: where do we say yes or no
 take_80 = false
-take_90 = false
+take_90 = true
 take_100 = true
+puts take_90
+
+#reassign
+hotel = "Plaza"
+amount = 12
+desire = 110000
+take_90 = false
+
+puts person_1
+puts take_90
+puts hotel
