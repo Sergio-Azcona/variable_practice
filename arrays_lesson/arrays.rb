acc128acaa834bb7b7b405152a99b396af9bda6e
Sergio Azconan <sergio.j.azcona@gmail.com>
2022-06-08 15:07:48 -0400

    Added calls and expected outcomes of methods

--- arrays_lesson/arrays.rb
+++ arrays_lesson/arrays.rb
@@ -3,3 +3,15 @@ home_bodies = ["meg","SA", "LJ", "Ari"]
 ages = [37, 38, 4, 2]
 months = [.6, .11, .9, .7]
 male = [false, true, true, true]
+
+#pop will return the last element, i.e. Ari
+home_bodies.pop
+
+#push will add 81 to the end of the array
+ages.push(81)
+
+#unshift will add .12 to the start of the month array
+month.unshift(.12)
+
+#shift will return the value sited, ie false
+male.shift(false)
